db03150f8704a883dcca8dc52f4cc9be7905e3e7
Rafael Oliveira <103541128+rafaloliveira@users.noreply.github.com>
2025-05-20 15:05:34 -0300

    Descreva aqui o que foi alterado

--- teste2.py
+++ teste2.py
@@ -531,7 +531,7 @@ with aba2:
                         <strong>Motorista:</strong> {ocorr.get('motorista', '-')}<br>
                         <strong>Tipo:</strong> {ocorr.get('tipo_de_ocorrencia', '-')}<br>
                         <strong>Aberto por:</strong> {ocorr.get('responsavel', '-')}<br>
-                        <strong>Data Abertura:</strong> {datetime.strptime(data_abertura_manual, '%Y-%m-%d').strftime('%d-%m-%Y') if data_abertura_manual else 'Não informada'}<br>
+                        <strong>Data Abertura:</strong> {data_abertura_manual or 'Não informada'}<br>
                         <strong>Hora Abertura:</strong> {hora_abertura_manual or 'Não informada'}<br> 
                         <strong>Observações:</strong> {ocorr.get('observacoes', 'Sem observações.')}<br>
                         </div>
@@ -541,8 +541,7 @@ with aba2:
 
                 with st.expander("Finalizar Ocorrência"):
                     data_atual = datetime.now().strftime("%d-%m-%Y")
-                    hora_atual = (datetime.utcnow() - timedelta(hours=3)).strftime("%H:%M")  # Sugerido em UTC−3
-
+                    hora_atual = datetime.now().strftime("%H:%M")
                     data_finalizacao_manual = st.text_input("Data Finalização (DD-MM-AAAA)", value=data_atual, key=f"data_final_{safe_idx}")
                     hora_finalizacao_manual = st.text_input("Hora Finalização (HH:MM)", value=hora_atual, key=f"hora_final_{safe_idx}")
 
