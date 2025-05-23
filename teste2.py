0607e2dfb49d1518ddae1def0c89b9f61689203a
Rafael Oliveira <103541128+rafaloliveira@users.noreply.github.com>
2025-05-22 23:35:06 -0300

    Descreva aqui o que foi alterado

--- teste2.py
+++ teste2.py
@@ -414,10 +414,20 @@ with aba1:
             st.markdown("")
 
             col_data, col_hora = st.columns(2)
+            agora_brasil = obter_data_hora_atual_brasil()
+
             with col_data:
-                data_abertura_manual = st.date_input("Data de Abertura", format="DD/MM/YYYY")
+                data_abertura_manual = st.date_input(
+                    "Data de Abertura",
+                    value=agora_brasil.date(),
+                    format="DD/MM/YYYY"
+                )
+
             with col_hora:
-                hora_abertura_manual = st.time_input("Hora de Abertura")
+                hora_abertura_manual = st.time_input(
+                    "Hora de Abertura",
+                    value=agora_brasil.time()
+                )
 
 
         enviar = st.form_submit_button("Adicionar Ocorrência")
