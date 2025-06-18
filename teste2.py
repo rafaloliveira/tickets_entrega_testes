be6bc4d9043ea147c7f0e7612521c9e113c80c7a
Rafael Oliveira <103541128+rafaloliveira@users.noreply.github.com>
2025-06-18 10:39:24 -0300

    Atualização

--- teste2.py
+++ teste2.py
@@ -1441,13 +1441,14 @@ with aba3:
     with col_titulo:
         st.header("Ocorrências Finalizadas")
     with col_botao:
-        
-        if st.button("🔄 Atualizar", key="btn_atualizar_finalizadas", use_container_width=True):
-            st.cache_data.clear()
-            st.session_state.ocorrencias_finalizadas = None
-            ocorrencias_finalizadas = carregar_ocorrencias_finalizadas()
-        else:
-            ocorrencias_finalizadas = carregar_ocorrencias_finalizadas()
+        atualizar = st.button("🔄 Atualizar", key="btn_atualizar_finalizadas", use_container_width=True)
+
+    if atualizar or "ocorrencias_finalizadas" not in st.session_state:
+        st.cache_data.clear()
+        st.session_state.ocorrencias_finalizadas = carregar_ocorrencias_finalizadas()
+
+    ocorrencias_finalizadas = st.session_state.ocorrencias_finalizadas
+
 
 
     try:
