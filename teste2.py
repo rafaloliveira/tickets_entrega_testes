3cc3f454e0941a50579580ff3c85765b24fe7abb
Rafael Oliveira <103541128+rafaloliveira@users.noreply.github.com>
2025-06-18 16:30:53 -0300

    Atualização

--- teste2.py
+++ teste2.py
@@ -60,10 +60,11 @@ if not cookies.ready():
 # --- Função para verificar se o cookie expirou ---
 def is_cookie_expired(expiry_time_str):
     try:
-        expiry_time = datetime.strptime(expiry_time_str, "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
+        expiry_time = datetime.datetime.strptime(expiry_time_str, "%Y-%m-%d %H:%M:%S").replace(tzinfo=datetime.timezone.utc)
     except ValueError:
         return False
-    return datetime.now(timezone.utc) > expiry_time
+    return datetime.datetime.now(datetime.timezone.utc) > expiry_time
+
 
 
 # --- Função de autenticação ---
